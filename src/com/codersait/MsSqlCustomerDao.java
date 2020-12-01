4e782b831b3307197dc231e05fa95ea51bd7d32b
saitg <codersait@gmail.com>
2020-12-01 13:46:42 +0300

    Text Injection

--- src/com/codersait/MsSqlCustomerDao.java
+++ src/com/codersait/MsSqlCustomerDao.java
@@ -1,8 +1,15 @@
 package com.codersait;
 
 public class MsSqlCustomerDao implements ICustomerDao{
+    private String connectionString;
+
+    public void setConnectionString(String connectionString) {
+        this.connectionString = connectionString;
+    }
+
     @Override
     public void add() {
+        System.out.println("Connection String: "+connectionString);
         System.out.println("MsSql veritabanina eklendi");
     }
 }
--- src/com/codersait/MySqlCustomerDao.java
+++ src/com/codersait/MySqlCustomerDao.java
@@ -1,9 +1,15 @@
 package com.codersait;
 
 public class MySqlCustomerDao implements ICustomerDao {
+    private String connectionString;
+
+    public void setConnectionString(String connectionString) {
+        this.connectionString = connectionString;
+    }
 
     @Override
     public void add() {
+        System.out.println("Connection String: "+connectionString);
         System.out.println("MySql veri tabanina eklendi");
     }
 }
